954989b2d04eb3d1e0c184c3daff838b2efb87f3
jose javier <josejavierpaez@hotmail.com>
2019-07-01 13:35:07 -0400

    first commit

---
+++ EventHandler/ArticleEventArgs.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventHandler
+{
+    internal class ArticleEventArgs : EventArgs
+    {
+        public ArticleEventArgs(double price)
+        {
+            Price = price;
+        }
+
+        public ArticleEventArgs()
+        {
+        }
+
+        public double Price { get; set; }
+
+        public event EventHandler<ArticleEventArgs> ChangeOfPrice;
+
+        public void OnChangePrice(ArticleEventArgs e)
+        {
+            Console.WriteLine("Ingrese el nuevo precio del artículo");
+            Price = Double.Parse(Console.ReadLine());
+
+            EventHandler<ArticleEventArgs> handler = ChangeOfPrice;
+
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+    }
+}
---
+++ EventHandler/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace EventHandler
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            double price;
+            Console.WriteLine("Ingrese el precio que tendrá el artículo");
+            price = Double.Parse(Console.ReadLine());
+
+            ArticleEventArgs article = new ArticleEventArgs(price);
+            Console.WriteLine($"El precio del artículo es: {article.Price}");
+
+            article.ChangeOfPrice += Article_ChangeOfPrice;
+
+            article.OnChangePrice(article);
+
+            Console.ReadKey();
+        }
+
+        private static void Article_ChangeOfPrice(object sender, ArticleEventArgs e)
+        {
+            Console.WriteLine("El precio del artículo fue alterado");
+            Console.WriteLine($"El nuevo precio es:  {e.Price}");
+        }
+    }
+}
